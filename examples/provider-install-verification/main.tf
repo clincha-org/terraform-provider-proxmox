d542f08144f88d3e903fd7ac687adc99bc365d0e
clincha <angus.clinch@gmail.com>
2024-07-28 16:10:54 +0100

    updated tests and README

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -12,15 +12,17 @@ provider "proxmox" {
   password = "vagrant"
 }
 
-data "proxmox_node" "edu" {}
+data "proxmox_node" "edu" {
+
+}
 
 output "edu_nodes" {
   value = data.proxmox_node.edu
 }
 
-resource "proxmox_network" "vmbr88" {
+resource "proxmox_network_bridge" "vmbr88" {
   interface = "vmbr88"
-  type      = "bridge"
+  node      = "pve"
   address   = "10.1.2.3"
   netmask   = "255.255.255.0"
   autostart = false
@@ -28,5 +30,5 @@ resource "proxmox_network" "vmbr88" {
 }
 
 output "vmbr88" {
-  value = proxmox_network.vmbr88
+  value = proxmox_network_bridge.vmbr88
 }
